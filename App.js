3cff8ef828bf0ed855e4b272b4863af3aa724f35
eme92 <angel92-eme@hotmail.com>
2021-09-02 13:49:13 -0500

    Navegacion Agregada

--- App.js
+++ App.js
@@ -1,33 +1,18 @@
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
-import HomeScreen from './homeScreen';
 
-function DetailsScreen({ navigation }) {
-  return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Details Screen</Text>
-      <Button
-        title="Go to Home... again"
-        onPress={() => navigation.navigate('Home')}
-      />
+export default function App(){
+  return(
+    <View style={styles.container}>
     </View>
-  );
+  )
 }
 
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="Details" component={DetailsScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
-  );
-}
-
-export default App;
+const styles = StyleSheet.create({
+  container: {
+    width: '100%',
+    height: '100%',
+    backgroundColor: '#aaa'
+  }
+});

cfe6df84d753c291075fc33360f44441b112f0bc
eme92 <angel92-eme@hotmail.com>
2021-09-03 00:39:41 -0500

    Navegacion Agregada

--- App.js
+++ App.js
@@ -1,20 +1,24 @@
 import React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 
+import {NavigationContainer} from '@react-navigation/native';
+
 import Header from './src/Components/Header/header.js';
 
 export default function App(){
   return(
+    <NavigationContainer>
     <View style={styles.container}>
       <Header/>
     </View>
+    </NavigationContainer>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
     width: '100%',
-    height: '100%',
-    backgroundColor: '#aaa'
+    height: '100vh',
+    backgroundColor: '#555'
   }
 });
---
+++ src/Components/Pages/Training/training.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text} from 'react-native';
+
+import styles from './trainingStyle';
+
+export default function Training(){
+    return(
+        <View style={styles.container}>
+            <Text>Training Working ...</Text>
+        </View>
+    )
+}

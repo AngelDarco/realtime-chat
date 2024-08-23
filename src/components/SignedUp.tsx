2deaa4fa116b63db5b842fe7848f1f4a0d9e235d
angeldarco <angel92-eme@hotmail.com>
2024-08-23 12:13:48 -0500

    :lipstick: signup ux design updated

--- src/components/SignedUp.tsx
+++ src/components/SignedUp.tsx
@@ -21,21 +21,28 @@ export default function SignUp() {
     const response = await sessions.signup(userData, dispatch);
     if (typeof response === "string") {
       userData["uid"] = response;
-      users.write(userData as SignOutUserData);
+      users.write(userData as SignOutUserData, null);
     }
   };
   return (
     <form
       onSubmit={handlerLogin}
-      className="w-full h-full flex flex-col items-center justify-center [&>label]:text-center [&>input]:rounded [&>input]:bg-inherit [&>input]:border [&>input]:p-1 [&>input]:mb-4"
+      className="w-full h-full flex flex-col items-center justify-center 
+       [&>label]:w-full [&>label]:pl-10 [&>label]:font-bold [&>label]:italic
+      [&>input]:rounded [&>input]:bg-inherit [&>input]:border-b-2 [&>input]:p-1 [&>input]:mb-4"
     >
-      <label htmlFor="name">name</label>
+      <label htmlFor="name">name:</label>
       <input name="name" type="text" />
-      <label htmlFor="email">email</label>
+      <label htmlFor="email">email:</label>
       <input name="email" type="text" />
-      <label htmlFor="password">password</label>
+      <label htmlFor="password">password:</label>
       <input name="password" type="password" />
-      <button className="py-1 px-5 mt-3 border border-white rounded">
+      <button
+        className="
+      py-1 px-5 mt-3 border rounded-lg
+      hover:font-bold
+      hover:animate-wiggle hover:animate-once hover:animate-duration-[1000ms]"
+      >
         login
       </button>
     </form>

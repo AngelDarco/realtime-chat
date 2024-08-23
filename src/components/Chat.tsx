a62b39461ecf663b4081b10a96637ada1f2ca3b6
angeldarco <angel92-eme@hotmail.com>
2024-08-23 16:32:01 -0500

    :lipstick: chatsroom ux updated

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -7,13 +7,19 @@ import { MessageRetrieve } from "../types";
 export default function Home() {
   const uid = useSelector((state: RootState) => state.session.uid);
   const uidTo = useSelector((state: RootState) => state.uidTo.uid);
-  const [, navigate] = useLocation();
+  const [location, navigate] = useLocation();
 
   const messageDB = new FirebaseRealtimeDatabase();
 
   const [messages, setMessages] = useState<MessageRetrieve[]>();
   const messageRef = useRef<HTMLInputElement>(null);
   const messagesContainerRef = useRef<HTMLDivElement>(null);
+
+  // go to the main page if there is no uid
+  useEffect(() => {
+    if (!uid) navigate("/");
+    if (uid && !uidTo) navigate("/users");
+  }, [uid, uidTo, location]);
 
   // get database messages
   useEffect(() => {
@@ -30,7 +36,7 @@ export default function Home() {
         handlerSendMessages();
       }
     });
-    return () => messageRef.current?.removeEventListener("keyup", () => {});
+    // return () => messageRef.current?.removeEventListener("keyup", () => {});
   }, []);
 
   // send the messages
@@ -61,46 +67,58 @@ export default function Home() {
     });
   }, [messages]);
 
+  // convert timestamp time to time ago
+  const handlerSendMessagesTime = (timestamp: string) => {
+    const time = new Date(+timestamp);
+    return time.toLocaleDateString() + " / " + time.toLocaleTimeString();
+  };
+
   return (
     <>
       {!uid || !uidTo ? (
         <></>
       ) : (
-        <div className="h-[90%]">
-          <div className="w-full h-full">
-            <div
-              ref={messagesContainerRef}
-              className="w-full h-full p-2 pt-10 flex items-end flex-col overflow-y-scroll"
-            >
-              {messages &&
-                messages.map((item, index) => {
-                  return (
-                    <div
-                      key={index}
-                      className={`w-full flex items-center ${
-                        item.uid === uid ? " justify-end" : " justify-start"
-                      }
+        <div className="w-full h-full flex flex-col">
+          <div
+            ref={messagesContainerRef}
+            className="w-full h-full p-2 pt-10 flex items-end flex-col overflow-y-scroll  no-scrollbar"
+          >
+            {messages &&
+              messages.map((item, index) => {
+                return (
+                  <div
+                    key={index}
+                    className={`w-full flex items-center ${
+                      item.uid === uid
+                        ? " justify-end [&>p]:bg-[#d3d3d3]"
+                        : "justify-start [&>p]:bg-[#a2a8d3]"
+                    }
                     `}
+                  >
+                    <p
+                      className={`border p-[2px] px-3 m-1 rounded-lg text-center border-none flex flex-col`}
                     >
-                      <span
-                        className={`border p-[2px] px-3 m-1 rounded-md text-center`}
-                      >
-                        {item.message}
+                      {item.message}
+                      <span className="w-full text-end text-[8px] text-gray-600">
+                        {handlerSendMessagesTime(item.timestamp)}
                       </span>
-                    </div>
-                  );
-                })}
-            </div>
-            <div className="w-full flex">
-              <input
-                ref={messageRef}
-                className="w-full outline-none text-indigo-700 px-2"
-                type="text"
-              />
-              <button onClick={handlerSendMessages} className="p-1 px-3">
-                send
-              </button>
-            </div>
+                    </p>
+                  </div>
+                );
+              })}
+          </div>
+          <div className="glass w-full flex">
+            <input
+              ref={messageRef}
+              className="w-full outline-none text-indigo-700 p-1 px-2 bg-inherit border-[1px] hover:bottom-2"
+              type="text"
+            />
+            <button
+              onClick={handlerSendMessages}
+              className="p-1 px-3 border text-orange-600 hover:text-green-600 hover:font-bold"
+            >
+              send
+            </button>
           </div>
         </div>
       )}
